70ab1eb0ebaf50feca718a98f26802fcbe5b7581
Abhishek bagdiya <Abhishekbagdiya94@gmail.com>
2023-09-14 23:03:48 +0530

    add insertion method in single linked list

--- linked list/single_linked_list/Main.java
+++ linked list/single_linked_list/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         SingleLinkedList sll = new SingleLinkedList();
-        sll.createSingleLinkedList(5);
+
+        sll.insertElement(34, 1);
         System.out.println(sll.head.value);
+        sll.insertElement(334, 3);
+        System.out.println(sll.head.next.value);
+
     }
 }
--- linked list/single_linked_list/SingleLinkedList.java
+++ linked list/single_linked_list/SingleLinkedList.java
@@ -1,7 +1,7 @@
 public class SingleLinkedList {
     public Node head;
     public Node tail;
-    int size;
+    static int size;
 
     public Node createSingleLinkedList(int nodeValue) {
 
@@ -14,7 +14,35 @@ public class SingleLinkedList {
         head = node;
         tail = node;
         size = 1;
-        // System.out.println(nodeValue);
+
         return head;
+    }
+
+    public void insertElement(int nodeValue, int location) {
+        Node node = new Node();
+        node.value = nodeValue;
+        if (head == null) {
+            createSingleLinkedList(nodeValue);
+            return;
+        } else if (location == 0) {
+            node.value = nodeValue;
+            head = node;
+        } else if (location >= size) {
+            node.next = null;
+            tail.next = node;
+            tail = node;
+        } else {
+            int index = 0;
+            Node tempNode = head;
+
+            while (index < location - 1) {
+                tempNode.next = node;
+                index++;
+            }
+            Node nextNode = tempNode.next;
+            tempNode.next = node;
+            node.next = nextNode;
+        }
+        size++;
     }
 }
