273f606fc389ef32976476c9c471a79bd0390f49
Abhishek bagdiya <Abhishekbagdiya94@gmail.com>
2023-09-18 22:26:39 +0530

    Circular singly linkedlist - insertion method

--- linked list/circular_singly_linked_list/Main.java
+++ linked list/circular_singly_linked_list/Main.java
@@ -3,7 +3,9 @@ public class Main {
         CircularSinglyLinkedList csll = new CircularSinglyLinkedList();
 
         csll.createCSLL(5);
+        csll.insertionInCSLL(2, 0);
 
+        System.out.println("Size is : " + csll.size);
         System.out.println(csll.head.value);
     }
 }
