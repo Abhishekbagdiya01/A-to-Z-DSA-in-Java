c83ea32839e11afc3cfce45485fe8ff740888c0a
Abhishek bagdiya <Abhishekbagdiya94@gmail.com>
2023-10-04 21:50:28 +0530

    Circular Doubly LinkedList - insertion method

--- linked list/circular_doubly_linked_list/CircularDoublyLinkedList.java
+++ linked list/circular_doubly_linked_list/CircularDoublyLinkedList.java
@@ -3,6 +3,7 @@ public class CircularDoublyLinkedList {
     public Node tail;
     public int size;
 
+    // Creation of Circular Doubly LinkedList
     public Node createCDLL(int nodeValue) {
 
         if (head == null) {
@@ -21,4 +22,42 @@ public class CircularDoublyLinkedList {
         }
         return head;
     }
+
+    // Insertion in Circular Doubly LinkedList
+
+    public void insertInDCLL(int nodeValue, int location) {
+        Node node = new Node();
+        node.value = nodeValue;
+
+        if (head == null) {
+            createCDLL(nodeValue);
+            return;
+        } else if (location == 0) {
+            node.prev = node;
+            node.next = head;
+            head.prev = node;
+
+            head = node;
+            tail.next = head;
+        } else if (location >= size) {
+            node.prev = tail;
+            node.next = head;
+            tail.next = node;
+            tail = node;
+
+        } else {
+            Node tempNode = head;
+            for (int i = 0; i < location - 1; i++) {
+                tempNode = tempNode.next;
+            }
+            node.prev = tempNode;
+            node.next = tempNode.next;
+            node.next.prev = node;
+            tempNode.next = node;
+
+        }
+
+        size++;
+    }
+
 }
--- linked list/circular_doubly_linked_list/Main.java
+++ linked list/circular_doubly_linked_list/Main.java
@@ -3,7 +3,10 @@ public class Main {
         CircularDoublyLinkedList cDLL = new CircularDoublyLinkedList();
 
         cDLL.createCDLL(14);
-        System.out.println(cDLL.head.value);
+        // System.out.println(cDLL.tail.value);
+        cDLL.insertInDCLL(78, 1);
+        cDLL.insertInDCLL(5, 2);
 
+        System.out.println(cDLL.head.next.next.value);
     }
 }
