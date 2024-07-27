8187bff491739ae564d832a580ce1c3f5367009b
Abhishek bagdiya <abhishekbagdiya94@gmail.com>
2024-07-27 17:16:29 +0530

    Hashing, Direct chaining

---
+++ Hashing/DirectChaining.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;
+public class DirectChaining {
+    LinkedList<String>[] hashTable;
+
+    @SuppressWarnings("unchecked")
+    DirectChaining(int size){
+        hashTable = new LinkedList[size];
+    }
+    
+    public int assaciHashFuntion(String word,int m){
+        char[] ch = word.toCharArray();
+        int total=0;
+        for (int i = 0; i < ch.length; i++) {
+            total += ch[i];
+        }
+        return total % m;
+    }
+
+    public void insertInHashTable(String word){
+        int index = assaciHashFuntion(word,hashTable.length);
+        if (hashTable[index]== null) {
+           hashTable[index] = new LinkedList<String>();
+           hashTable[index].add(word);
+        }else{
+            hashTable[index].add(word);
+        }
+    }
+    public void traverseInHashTable(){
+        if (hashTable == null) {
+            System.out.println("Hashtable doesn't exist");
+        }
+        else{
+            for (int i = 0; i < hashTable.length; i++) {
+                System.out.println("Index "+ i+ ", Key :" +hashTable[i]);
+            }
+        }
+        }
+}
---
+++ Hashing/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        DirectChaining directChaining = new DirectChaining(15);
+       directChaining.insertInHashTable("Hii");
+       directChaining.insertInHashTable("How are you");
+       directChaining.insertInHashTable("ohh i see");
+       directChaining.insertInHashTable("SPD Emergrncy");
+       directChaining.insertInHashTable("I chose you pikachu");
+       directChaining.insertInHashTable("I am Batman");
+       directChaining.insertInHashTable("Wubba lubba dub dub");
+       directChaining.insertInHashTable("Bonk");
+       directChaining.insertInHashTable("That's make some sense");
+       directChaining.insertInHashTable("i am out of my convo skill");
+       directChaining.insertInHashTable("No Cap");
+       directChaining.traverseInHashTable();
+    }
+}
