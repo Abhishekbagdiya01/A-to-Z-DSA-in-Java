508e2ca86c0dab85fcf6b8db6e06e5f5b3b32909
Abhishek bagdiya <abhishekbagdiya94@gmail.com>
2024-10-15 23:28:22 +0530

    implemented Pre-Order Traversal in Binary tree

--- Tree/Binary Tree/BinaryTreeLL.java
+++ Tree/Binary Tree/BinaryTreeLL.java
@@ -1,6 +1,17 @@
 public class BinaryTreeLL {
-  BinaryNode root ;
-  BinaryTreeLL(){
+  BinaryNode root;
+
+  BinaryTreeLL() {
     this.root = null;
+  }
+
+  // Pre-Order Traversal
+  void preOrder(BinaryNode node) {
+    if (node == null){
+      return;
+    }
+    System.out.print(node.value + " ");
+    preOrder(node.leftChild);
+    preOrder(node.rightChild);
   }
 }
--- Tree/Binary Tree/Main.java
+++ Tree/Binary Tree/Main.java
@@ -1,5 +1,40 @@
 public class Main {
   public static void main(String[] args) {
     BinaryTreeLL binaryTree = new BinaryTreeLL();
+    BinaryNode n1 = new BinaryNode();
+    n1.value = "N1";
+    BinaryNode n2 = new BinaryNode();
+    n2.value = "N2";
+    BinaryNode n3 = new BinaryNode();
+    n3.value = "N3";
+    BinaryNode n4 = new BinaryNode();
+    n4.value = "N4";
+    BinaryNode n5 = new BinaryNode();
+    n5.value = "N5";
+    BinaryNode n6 = new BinaryNode();
+    n6.value = "N6";
+    BinaryNode n7 = new BinaryNode();
+    n7.value = "N7";
+    BinaryNode n8 = new BinaryNode();
+    n8.value = "N8";
+    BinaryNode n9 = new BinaryNode();
+    n9.value = "N9";
+
+
+    n1.leftChild = n2;
+    n1.rightChild = n3;
+
+    n2.leftChild = n4;
+    n2.rightChild = n5;
+
+    n3.leftChild = n6;
+    n3.rightChild = n7;
+
+    n4.leftChild = n8;
+    n4.rightChild = n9;
+
+    binaryTree.root = n1;
+    binaryTree.preOrder(binaryTree.root);
+
   }
 }
